ca3ec5bd5a24222012642292a098ef4f2abaf94b
Alex Nunez <alexnunez957@gmail.com>
2022-01-15 20:35:40 -0800

    trying to add new ports

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,33 @@
-import React from "react";
-import PortfolioContainer from "./components/PortfolioContainer";
+import React, { useState } from "react";
 import "./App.css";
+import About from "../src/components/pages/About";
+import Contact from "../src/components/pages/Contact";
 
-const App = () => <PortfolioContainer />;
+function App() {
+  const [currentPage, setCurrentPage] = useState("About Me");
 
+  // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
+  const renderPage = () => {
+    if (currentPage === "About Me") {
+      return <About />;
+    }
+    if (currentPage === "Contact") {
+      return <Contact />;
+    }
+    if (currentPage === "Portfolio") {
+      return <Portfolio />;
+    }
+    return <Resume />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
+      <Footer />
+    </div>
+  );
+}
 export default App;
