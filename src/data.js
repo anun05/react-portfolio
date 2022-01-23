18f60b1b75d3fa2c5cb8ae32c3bb1bbcb4beeac9
Alex Nunez <alexnunez957@gmail.com>
2022-01-23 10:21:02 -0800

    added images

--- src/data.js
+++ src/data.js
@@ -3,7 +3,8 @@ export const projects = [
     title: "Work Day Schedule",
     description:
       "The goal for this application was intended for those to write down their schedule. The grey meant it was the past, the red means the current time, and the green depicts future planned schedules.",
-    image: "",
+    image:
+      "https://github.com/anun05/react-portfolio/blob/main/src/images/Screenshot%202022-01-18%20125724.png?raw=true",
     link: "https://anun05.github.io/Work-schedule-hw/",
     subtitle: "https://github.com/anun05/Work-schedule-hw",
   },
@@ -20,7 +21,8 @@ export const projects = [
     title: "Team Profile Generator",
     description:
       "The following link will provide a video demo, however, the purpose of this application is to use this by running node index.js to the command line and answer the following questions prompted. After all question's have been asnwered, an html page is genertaed that prompts the user with different cards with different employee information.",
-    image: "",
+    image:
+      "https://github.com/anun05/react-portfolio/blob/main/src/images/Screenshot%202022-01-18%20133038.png?raw=true",
     link: "https://watch.screencastify.com/v/MYcAV2hLfq2sj5vHmrZt",
     subtitle: "https://github.com/anun05/team-profile-generator",
   },
@@ -28,7 +30,8 @@ export const projects = [
     title: "NFT-Generator",
     description:
       "The purpose for our project is so people can experince the wolrd of NFT's and make/but their own. This was made by John des Garennes, Israel Rios, and myself!",
-    image: "",
+    image:
+      "https://github.com/anun05/react-portfolio/blob/main/src/images/Screenshot%202021-11-29%20174219.png?raw=true",
     link: "https://nft-generator101.herokuapp.com/",
     subtitle: "https://github.com/anun05/nft-generator",
   },
