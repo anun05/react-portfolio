b10147996869b595ec6dd6dd9d1ca92b49472a98
Alex Nunez <alexnunez957@gmail.com>
2022-01-23 10:10:27 -0800

    took out copied files

--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ export const projects = [
     title: "Work Day Schedule",
     description:
       "The goal for this application was intended for those to write down their schedule. The grey meant it was the past, the red means the current time, and the green depicts future planned schedules.",
-    image: "./images/Screenshot 2022-01-18 125724.png",
+    image: "",
     link: "https://anun05.github.io/Work-schedule-hw/",
     subtitle: "https://github.com/anun05/Work-schedule-hw",
   },
@@ -11,7 +11,8 @@ export const projects = [
     title: "Password Generator",
     description:
       "The goal for this application is to generate a random password for those looking for new a password. The generator gathers random numbers together and spits something out that follows the users choosen guide lines. ",
-    image: "./images/Screenshot 2021-10-06 142945.png",
+    image:
+      "https://github.com/anun05/react-portfolio/blob/main/src/images/Screenshot%202021-10-06%20142945.png?raw=true",
     link: "https://anun05.github.io/Homework-3/",
     subtitle: "https://github.com/anun05/Homework-3",
   },
@@ -19,7 +20,7 @@ export const projects = [
     title: "Team Profile Generator",
     description:
       "The following link will provide a video demo, however, the purpose of this application is to use this by running node index.js to the command line and answer the following questions prompted. After all question's have been asnwered, an html page is genertaed that prompts the user with different cards with different employee information.",
-    image: "./images/Screenshot 2022-01-18 133038.png",
+    image: "",
     link: "https://watch.screencastify.com/v/MYcAV2hLfq2sj5vHmrZt",
     subtitle: "https://github.com/anun05/team-profile-generator",
   },
@@ -27,7 +28,7 @@ export const projects = [
     title: "NFT-Generator",
     description:
       "The purpose for our project is so people can experince the wolrd of NFT's and make/but their own. This was made by John des Garennes, Israel Rios, and myself!",
-    image: "./images/Screenshot 2021-11-29 174311.png",
+    image: "",
     link: "https://nft-generator101.herokuapp.com/",
     subtitle: "https://github.com/anun05/nft-generator",
   },
